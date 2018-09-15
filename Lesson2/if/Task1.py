fe3c86bcfff30aeb78c4c7bbd34e7f5e6ad792ed
Spreys Alexander <spreys.a@mail.ru>
2018-09-15 14:00:24 +0300

    Closes #2

--- Lesson2/if/Task1.py
+++ Lesson2/if/Task1.py
@@ -1,18 +1,21 @@
 #Практика: Возраст
-age = input("Enter your age: ")
+age = float(input("Enter your age: "))
 
 
 def who_is_user(user_age):
+    float(user_age)
+
     if user_age < 5:
-        print("Your age is " + str(user_age) + ". You must be pupil of a kindergarten!")
+        resp = "Your age is " + str(user_age) + ". You must be pupil of a kindergarten!"
     elif user_age < 16:
-        print("Your age is " + str(user_age) + ". You must be a schoolboy!")
+        resp = "Your age is " + str(user_age) + ". You must be a schoolboy!"
     elif user_age < 22:
-        print("Your age is " + str(user_age) + ". You must be a student!")
+        resp = "Your age is " + str(user_age) + ". You must be a student!"
     else:
-        print("Your age is " + str(user_age) + ". It is time to start working!")
+        resp = "Your age is " + str(user_age) + ". It is time to start working!"
+    return resp
 
 
-who_is_user(int(age))
+print(who_is_user(age))
 
 
